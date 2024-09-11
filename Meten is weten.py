db544c8704fe960762204d4d5f7596989a22af56
Mikevdmeer <99081100@mydavinci.nl>
2024-09-11 13:31:52 +0200

    elif-statement

--- Meten is weten.py
+++ Meten is weten.py
@@ -1,5 +1,8 @@
 a = int(input("Noem getal 1 op: "))
 b = int(input("Noem getal 2 op: "))
 Max = a
+Min = a
 if a > b:
-    print(f"A is groter, {Max}")
+    print(f"A is het grootste getal: {Max}")
+elif b > a:
+    print(f"a is het kleinste getal: {Min}")
