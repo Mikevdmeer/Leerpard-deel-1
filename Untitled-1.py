e67e8b2ba06ae8b25279bf5e7985d9e19717061e
Mikevdmeer <99081100@mydavinci.nl>
2024-09-11 13:14:29 +0200

    commit

---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+naam = input("Hallo, wat is je naam? ")
+leeftijd = input(f"Goedendag {naam}, hoe oud ben je? ")
+favoriete_eten = input(f"En als {leeftijd} jarige, wat eet je het liefst? ")
+favoriete_drinken = input(f"En wat drink je het liefst bij {favoriete_eten}? ")
+
+print(f"De {leeftijd} jarige {naam} drinkt het liefst {favoriete_drinken} bij {favoriete_eten}")
