cba1b3bbd9bcb72e6b0903806b510a0e585fd5e1
Mikevdmeer <99081100@mydavinci.nl>
2024-09-13 11:25:57 +0200

    party opdracht aangepast

--- startTheParty.py
+++ startTheParty.py
@@ -1,41 +1,55 @@
-gastheer = True
+gastheer = input("Wie is de gastheer? ")
+
 gasten = True
 drank = True
 chips = True
 
-#Een feest moet minimaal gasten of een gastheer hebben.
-start_condition_1 = gastheer or gasten
-start_condition_2 = gastheer or (gasten and chips and drank)
+jouw_naam = "Mike"
+slb_naam = "Oorschot"
+
+start_condition_1 = (gastheer != "") or gasten
+start_condition_2 = (gastheer != "") or (gasten and chips and drank)
 start_condition_3 = not (chips and not drank)
 start_condition_4 = not (gasten and (not chips or not drank))
-start_condition_5 = gastheer and drank
-start_condition_6 = not (chips and not gasten and not gastheer)
-if start_condition_1:
+start_condition_5 = (gastheer != "") and drank
+start_condition_6 = not (chips and not gasten and not (gastheer != ""))
+
+feest_hoe_dan_ook = (gastheer == jouw_naam)
+geen_feest_hoe_dan_ook = (gastheer == slb_naam)
+
+if feest_hoe_dan_ook:
     print('Start the Party')
-else:
+
+elif geen_feest_hoe_dan_ook:
     print('No Party')
 
-if start_condition_2:
-    print('Start the Party')
 else:
-    print('No Party')
+    if start_condition_1:
+        print('Start the Party')
+    else:
+        print('No Party')
 
-if start_condition_3:
-    print('Start the Party')
-else:
-    print('No Party') 
-    
-if start_condition_4:
-    print('Start the Party')
-else:
-    print('No Party') 
+    if start_condition_2:
+        print('Start the Party')
+    else:
+        print('No Party')
 
-if start_condition_5:
-    print('Start the Party')
-else:
-    print('No Party') 
+    if start_condition_3:
+        print('Start the Party')
+    else:
+        print('No Party')
 
-if start_condition_6:
-    print('Start the Party')
-else:
-    print('No Party') 
+    if start_condition_4:
+        print('Start the Party')
+    else:
+        print('No Party')
+
+    if start_condition_5:
+        print('Start the Party')
+    else:
+        print('No Party')
+
+    if start_condition_6:
+        print('Start the Party')
+    else:
+        print('No Party')
